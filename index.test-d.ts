5d9edb46fdee28715092f2ad98c78a1828e4980a
Oria <oriadam@gmail.com>
2021-02-02 11:10:47 +0200

    Update index.test-d.ts

--- index.test-d.ts
+++ index.test-d.ts
@@ -7,5 +7,4 @@ expectType<devtools.Orientation | undefined>(devtools.orientation);
 window.addEventListener('devtoolschange', devtoolsEvent => {
 	expectType<devtools.DevToolsEvent>(devtoolsEvent);
 	expectType<boolean>(devtoolsEvent.detail.isOpen);
-	expectType<devtools.Orientation | undefined>(devtoolsEvent.detail.orientation);
 });
